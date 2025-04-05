9d5b9c706695a18b236bd903263d392715f477d5
pathumck <ckpathumck@gmail.com>
2025-04-05 22:41:00 +0530

    create basic header

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import logo from "../assets/blogger.png";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+import { MdLogin } from "react-icons/md";
+import SearchBox from "./SearchBox";
 
 function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <div className="flex justify-between items-center h-16 fixed w-full z-20 bg-white px-5 border-b">
+      <div>
+        <img src={logo} width={40} alt="logo" />
+      </div>
+      <div  className='w-[500px]'>
+        <SearchBox />
+      </div>
+      <div>
+        <Button asChild>
+          <Link to="" className="rounded-full">
+            <MdLogin />
+            Sign In
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Topbar
+export default Topbar;
