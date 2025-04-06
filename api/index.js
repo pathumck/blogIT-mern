76b9bb834273a1c2b48b6b2bc70456f7f5ee1d5f
pathumck <ckpathumck@gmail.com>
2025-04-06 11:59:15 +0530

    create db connection

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,10 +10,21 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGO_DB_CON, { dbName: "mern-blog" })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
