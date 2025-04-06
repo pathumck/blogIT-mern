2c33309ec1948515ee597bd685a70b06b0cd6684
pathumck <ckpathumck@gmail.com>
2025-04-06 18:15:32 +0530

    create sign in

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { handleError } from "../helpers/handleError.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res, next) => {
   try {
@@ -26,4 +27,42 @@ export const Register = async (req, res, next) => {
   }
 };
 
-export const Login = async () => {};
+export const Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      next(handleError(404, "Invalid login credentials."));
+    }
+    const hashedPassword = user.password;
+    const comparePassword = bcrypt.compare(password, hashedPassword);
+    if (!comparePassword) {
+      next(handleError(404, "Invalid login credentials."));
+    }
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      },
+      process.env.JWT_SECRET
+    );
+    res.cookie('access_token', token, {
+      httpOnly :true,
+      secure : process.env.NODE_ENV === 'production',
+      sameSite : process.env.NODE_ENV === 'production'? 'none' : 'strict',
+      path:'/'
+    })
+
+    res.status(200).json ({
+      success:true,
+      user,
+      message : 'Login successful.'
+    })
+  } catch (error) {
+    next(handleError(500, error.message));
+  }
+};
