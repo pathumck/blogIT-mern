ddc16522924b879b09cddd060038215861c041a0
pathumck <ckpathumck@gmail.com>
2025-04-11 19:19:35 +0530

    create user dropdown menu

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -4,23 +4,78 @@ import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
 import { MdLogin } from "react-icons/md";
 import SearchBox from "./SearchBox";
+import { useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import profileImage from "../assets/avatar.png";
+import { FaRegUser } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa";
+import { IoLogOutOutline } from "react-icons/io5";
 
 function Topbar() {
+  const user = useSelector((state) => state.user);
   return (
     <div className="flex justify-between items-center h-16 fixed w-full z-20 bg-white px-5 border-b">
       <div>
         <img src={logo} width={40} alt="logo" />
       </div>
-      <div  className='w-[500px]'>
+      <div className="w-[500px]">
         <SearchBox />
       </div>
       <div>
-        <Button asChild className="rounded-full">
-          <Link to="/sign-in">
-            <MdLogin />
-            Sign In
-          </Link>
-        </Button>
+        {!user.isLoggedIn ? (
+          <Button asChild className="rounded-full">
+            <Link to="/sign-in">
+              <MdLogin />
+              Sign In
+            </Link>
+          </Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage
+                  src={user.user.avatar || profileImage}
+                  referrerPolicy="no-referrer"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                <p>{user.user.name}</p>
+                <p className="text-xs">{user.user.email}</p>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link to="/profile">
+                  <FaRegUser />
+                  Profile
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Link to="/create-post">
+                  <FaPlus />
+                  Create Blog
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                <Link to="/logout">
+                  <IoLogOutOutline color="red" />
+                  Logout
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </div>
   );
