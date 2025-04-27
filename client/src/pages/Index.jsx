b75dde38693521a80c51e33ff581f7262d91a572
pathumck <ckpathumck@gmail.com>
2025-04-27 17:33:22 +0530

    add home page title

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -4,6 +4,7 @@ import { getEnv } from "@/helpers/getEnv";
 import { useState, useEffect } from "react";
 import { useFetch } from "@/hooks/useFetch";
 import spinner from "../assets/scroll-spinner.svg";
+import { IoHomeOutline } from "react-icons/io5";
 
 function Index() {
   const [page, setPage] = useState(1);
@@ -31,27 +32,33 @@ function Index() {
   };
 
   return (
-    <InfiniteScroll
-      dataLength={blogs.length}
-      next={fetchMore}
-      hasMore={hasMore}
-      loader={
-        <div className="flex justify-center items-center">
-          {<img className="w-10" src={spinner} alt="" />}
-        </div>
-      }
-      endMessage={
-        <p className="text-center pt-5 text-orange-600 font-bold">
-          You have seen it all...
-        </p>
-      }
-    >
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {blogs.length > 0
-          ? blogs.map((blog) => <BlogCard key={blog._id} props={blog} />)
-          : !loading && <div>Data not found</div>}
+    <>
+      <div className="flex items-center gap-3 pb-2   text-orange-600">
+        <IoHomeOutline size={22} />
+        <h1 className="text-2xl font-bold mt-1">Home</h1>
       </div>
-    </InfiniteScroll>
+      <InfiniteScroll
+        dataLength={blogs.length}
+        next={fetchMore}
+        hasMore={hasMore}
+        loader={
+          <div className="flex justify-center items-center">
+            {<img className="w-10" src={spinner} alt="" />}
+          </div>
+        }
+        endMessage={
+          <p className="text-center pt-5 text-orange-600 font-bold">
+            You have seen it all...
+          </p>
+        }
+      >
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+          {blogs.length > 0
+            ? blogs.map((blog) => <BlogCard key={blog._id} props={blog} />)
+            : !loading && <div>Data not found</div>}
+        </div>
+      </InfiniteScroll>
+    </>
   );
 }
 
