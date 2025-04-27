0dcb97e4cf14f00c21b26d2691a53ed8282baea8
pathumck <ckpathumck@gmail.com>
2025-04-27 22:08:05 +0530

    solve loading error

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -11,22 +11,28 @@ function SearchResults() {
     data: blogData,
     loading,
     error,
-  } = useFetch(`${getEnv("VITE_API_BASE_URL")}/blog/search?q=${q}`, {
-    method: "GET",
-    credentials: "include",
-  }, [q]);
+  } = useFetch(
+    `${getEnv("VITE_API_BASE_URL")}/blog/search?q=${q}`,
+    {
+      method: "GET",
+      credentials: "include",
+    },
+    [q]
+  );
   console.log(blogData);
   return (
     <>
       <div className="flex items-center gap-3 pb-2  text-orange-600">
-        <h4 className="text-2xl font-bold text-orange-600">Search Results for : "{q}"</h4>
+        <h4 className="text-2xl font-bold text-orange-600">
+          Search Results for : "{q}"
+        </h4>
       </div>
       <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {blogData && blogData.data.length > 0 ? (
-          blogData.data.map((blog) => <BlogCard key={blog._id} props={blog} />)
-        ) : (
-          <div>Data not found</div>
-        )}
+        {blogData && blogData.data.length > 0
+          ? blogData.data.map((blog) => (
+              <BlogCard key={blog._id} props={blog} />
+            ))
+          : !loading && blogData.data.length === 0 && <div>Data not found</div>}
       </div>
     </>
   );
